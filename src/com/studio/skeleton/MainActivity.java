05c24cf99dc804adfc398adb95ba8c083774bda7
Tery Lim <kumabotz@gmail.com>
2014-12-14 00:21:01 +1300

    Remove debug textview

--- src/com/studio/skeleton/MainActivity.java
+++ src/com/studio/skeleton/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -45,11 +44,8 @@ public class MainActivity extends Activity {
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
-        TextView textView = (TextView) findViewById(R.id.info_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
-        textView.setText(message);
-        textView.setVisibility(View.VISIBLE);
         startActivity(intent);
     }
 }
